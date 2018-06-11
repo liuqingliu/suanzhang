2460fbefbea4376f03c31f90cb7a33394913529a
liuqing <liuqing@ludashi.com>
2018-06-11 17:37:21 +0800

    test1

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,12 +5,14 @@
 	 * Date: 2018/6/11
 	 * Time: 16:40
 	 */
+namespace App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
 
 class ApiController extends Controller
 {
 	public function getUserinfo(Request $request)
 	{
+		var_dump($request);exit;
 		return $request;
 	}
 
