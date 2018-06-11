4d124567f05ad6c4e90280fa6976bdcba7db375e
liuqing <liuqing@ludashi.com>
2018-06-11 17:29:54 +0800

    some config

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,17 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: dell
+	 * Date: 2018/6/11
+	 * Time: 16:40
+	 */
+use App\Http\Controllers\Controller;
+
+class ApiController extends Controller
+{
+	public function getUserinfo(Request $request)
+	{
+		return $request;
+	}
+
+}
