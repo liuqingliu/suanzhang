1f0b3713347b0d4290b2f07d09408a262a0a2ac6
liuqing <liuqing@ludashi.com>
2018-06-11 18:15:19 +0800

    test2

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: dell
+	 * Date: 2018/6/11
+	 * Time: 16:40
+	 */
+	namespace App\Http\Controllers\Api;
+
+	use App\Api\Helpers\Api\ApiResponse;
+	use App\Http\Controllers\Controller;
+
+	class ApiController extends Controller
+	{
+
+		use ApiResponse;
+
+		// 其他通用的Api帮助函数
+
+	}
