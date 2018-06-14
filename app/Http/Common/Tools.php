048c4b124cccb20d51c76dd241b7dc7a78da02ad
liuqing <liuqing@ludashi.com>
2018-06-14 14:22:12 +0800

    test7

--- app/Http/Common/Tools.php
+++ app/Http/Common/Tools.php
@@ -13,10 +13,10 @@ class Tools
 	public static function outPut($outData, $callback=false)
 	{
 		if($callback) {
-
+			echo $callback.'('.json_encode($outData).')';
 		}else{
 			echo json_encode($outData);
-			exit;
 		}
+		exit;
 	}
 }
